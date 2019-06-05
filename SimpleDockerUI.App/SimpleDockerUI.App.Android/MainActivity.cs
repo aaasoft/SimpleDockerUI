35874dc2054b5f32f61b7c6af08b5f0d90ec4d43
张鹏 <scbeta@qq.com>
2019-06-05 15:01:02 +0800

    修改APP的显示名称为SimpleDockerUI

--- SimpleDockerUI.App/SimpleDockerUI.App.Android/MainActivity.cs
+++ SimpleDockerUI.App/SimpleDockerUI.App.Android/MainActivity.cs
@@ -13,7 +13,7 @@ using Plugin.CurrentActivity;
 
 namespace SimpleDockerUI.App.Droid
 {
-    [Activity(Label = "SimpleDockerUI.App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "SimpleDockerUI", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
